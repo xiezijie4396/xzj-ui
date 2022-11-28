c068c3d72aa2e9728dd474ed450edf2f3d9a5511
xiezijie4396 <775826360@qq.com>
2022-11-28 15:53:54 +0800

    feat: 提交打包代码

--- packages/vueui/vite.config.js
+++ packages/vueui/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
 
 export const config = {
-  plugins: [vue()],
+  plugins: [
+    dts({
+      outputDir: "./dist/types",
+      insertTypesEntry: true, // 插入TS 入口
+      copyDtsFiles: true, // 是否将源码里的 .d.ts 文件复制到 outputDir
+    }),
+    vue(),
+  ],
   test: {
     globals: true,
     environment: "happy-dom",
